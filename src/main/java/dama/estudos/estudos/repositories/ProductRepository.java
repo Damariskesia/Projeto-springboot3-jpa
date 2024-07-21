4268341a9bbd0b4f4c15c2b9fd7aabd29956fe35
Dama <damariskvaz@hotmail.com>
2024-07-21 19:32:33 -0300

    Product - Many-to-many association with JoinTable

---
+++ src/main/java/dama/estudos/estudos/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package dama.estudos.estudos.repositories;
+
+import dama.estudos.estudos.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
