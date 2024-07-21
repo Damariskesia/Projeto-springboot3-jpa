093929564682812021ff844083f6eb0fa62e38d7
Dama <damariskvaz@hotmail.com>
2024-07-20 21:49:10 -0300

    H2 database, teste profile, JPA

--- src/main/java/dama/estudos/estudos/entities/User.java
+++ src/main/java/dama/estudos/estudos/entities/User.java
@@ -1,22 +1,28 @@
 package dama.estudos.estudos.entities;
 
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
     private String phone;
     private String password;
 
-    private User(){
+    public User(){
     }
 
     public User(long id, String name, String email, String phone, String password) {
